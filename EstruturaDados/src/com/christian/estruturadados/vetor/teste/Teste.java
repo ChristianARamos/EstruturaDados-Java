e878ce541e7c197f8db79d583b5efc1af8759dff
CAvila <christian.a.ramos@gmail.com>
2017-04-28 21:59:12 -0300

    First Commit
    Criando vetores.

---
+++ EstruturaDados/src/com/christian/estruturadados/vetor/teste/Teste.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.christian.estruturadados.vetor.teste;
+
+import com.christian.estruturadados.vetor.Vetor;
+
+/**
+ *
+ * @author Christian Ramos
+ */
+public class Teste {
+    public static void main(String[] args){
+        
+        Vetor vet = new Vetor(5);
+
+        int i = 0;
+        i++;
+        
+        vet.adiciona("A");
+        vet.adiciona("B");
+    }
+}
