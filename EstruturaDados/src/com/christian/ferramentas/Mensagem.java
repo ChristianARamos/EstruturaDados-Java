330a7008ed998a5837534aa792603174d42cfb4b
CAvila <christian.a.ramos@gmail.com>
2017-05-09 19:40:23 -0300

    Mensagem atual.

--- EstruturaDados/src/com/christian/ferramentas/Mensagem.java
+++ EstruturaDados/src/com/christian/ferramentas/Mensagem.java
@@ -8,6 +8,8 @@ package com.christian.ferramentas;
 import javax.swing.JOptionPane;
 
 /**
+ * A classe Mensagem exibe mensagens no console e também na tela através das
+ * classes System.out.println() e JOptionPane.showMessageDialog().
  *
  * @author Christian Ramos
  */
@@ -16,39 +18,53 @@ public class Mensagem {
     public Mensagem() {
 
     }
-    
-    /**Exibe uma mensagem do tipo String no console*/
+
+    /**
+     * Retorna uma mensagem do tipo String no console
+     */
     public static void imprimirMensagem(String msg) {
         System.out.println(msg);
     }
-    
-    /**Exibe uma mensagem do tipo String no console*/
+
+    /**
+     * Retorna uma mensagem do tipo String no console
+     */
     public static void imprimirMensagem(String msg, int var) {
         System.out.println(msg + var);
     }
 
-    /**Exibe uma mensagem do tipo String no console*/
+    /**
+     * Retorna uma mensagem do tipo String no console
+     */
     public static void imprimirMensagem(String msg, int var, String msg2) {
         System.out.println(msg + var + msg2);
     }
 
-    /**Exibe uma mensagem de erro na tela.*/
-    public static void exibirMensagemErro(String msg){
+    /**
+     * Retorna uma mensagem de erro na tela.
+     */
+    public static void exibirMensagemErro(String msg) {
         JOptionPane.showMessageDialog(null, msg, "<< ALERTA DE ERRO >>", 0);
     }
-    
-        /**Exibe uma informação na tela.*/
-    public static void exibirInformação(String msg){
+
+    /**
+     * Retorna uma informação na tela.
+     */
+    public static void exibirInformação(String msg) {
         JOptionPane.showMessageDialog(null, msg, "<< INFORMAÇÃO >>", 1);
     }
-    
-    /**Exibe uma mensagem de aviso na tela.*/
-    public static void exibirMensagemAviso(String msg){
+
+    /**
+     * Retorna uma mensagem de aviso na tela.
+     */
+    public static void exibirMensagemAviso(String msg) {
         JOptionPane.showMessageDialog(null, msg, "<< ATENÇÃO >>", 2);
     }
-    
-    /**Exibe uma mensagem de questionamento na tela.*/
-    public static void exibirMensagemQuestao(String msg){
+
+    /**
+     * Retorna uma mensagem de questionamento na tela.
+     */
+    public static void exibirMensagemQuestao(String msg) {
         JOptionPane.showMessageDialog(null, msg, "<< QUESTIONANDO >>", 3);
     }
 }
