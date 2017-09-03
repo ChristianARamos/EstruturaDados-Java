906660274972789349303e449a64178d11108eb5
CAvila <christian.a.ramos@gmail.com>
2017-09-03 16:07:55 -0300

    Generalizando
    Generaliza o código de forma que possa ser utilizado qualquer tipo.

--- EstruturaDados/src/com/christian/estruturadados/vetor/Vetor.java
+++ EstruturaDados/src/com/christian/estruturadados/vetor/Vetor.java
@@ -10,23 +10,42 @@ import com.christian.ferramentas.Mensagem;
 /**
  * Classe utilizada para contrução e manipulação de vetores.
  *
- * @author Christian Ramos
+ * @author Christian A. Ramos
+ * @param <T>
  */
-public class Vetor {
+public class Vetor<T> { // T de Type (Generics).
 
-    private String[] elementos;
+    private T[] elementos;
     private int tamanho;
 
     /**
+     * Contrutor da classe Vetor com capacidade inicial pré-definida em 10.
+     */
+    public Vetor(){
+    
+        this.elementos = (T[]) new Object[10];
+    }
+    
+    /**
      * Construtor da classe Vetor.
+     * @param capacidade 
      */
     public Vetor(int capacidade) {
-        this.elementos = new String[capacidade];
+        this.elementos = (T[]) new Object[capacidade];
+        this.tamanho = 0;
+        
+    }
+    
+    /**Outra solução possível para o construtor acima
+    public Vetor(int capacidade, Class<T> tipoClasse){
+        this.elementos = (T[]) Array.newInstance(tipoClasse, capacidade);
         this.tamanho = 0;
     }
-
+    */
+    
     /**
      * Retorna o tamanho real do vetor.
+     * @return 
      */
     public int tamanho() {
         return this.tamanho;
@@ -34,16 +53,30 @@ public class Vetor {
 
     /**
      * Retorna a capacidade do vetor.
+     * @return 
      */
     public int capacidadeVetor() {
         return this.elementos.length;
     }
 
+    /**Duplica a capacidade do vetor.*/
+    public void aumentaCapacidade(){
+        
+        if (this.tamanho == this.elementos.length){
+            T[] elementosNovos = (T[]) new Object[this.elementos.length + 10];
+            System.arraycopy(this.elementos, 0, elementosNovos, 0, this.elementos.length);
+            this.elementos = elementosNovos;
+        }
+    }
+    
     /**
      * Retorna true se um elemento for adicionado ao vetor.
+     * @param elemento
+     * @return 
      */
-    public boolean adicionarElemento(String elemento) {
+    public boolean adicionaElemento(T elemento) {
 
+        aumentaCapacidade();
         if (this.tamanho < this.elementos.length) {
             this.elementos[this.tamanho] = elemento;
             this.tamanho++;
@@ -55,12 +88,16 @@ public class Vetor {
 
     /**
      * Adiciona um elemento na posição desejada.
+     * @param elemento
+     * @param posicao
      */
     //inserir "A" na posição "0".
     //Vetor:   B C E F G
     //Posição: 0 1 2 3 4
     //tamanho = "5".
-    public void adicionarElemento(String elemento, int posicao) {
+    public void adicionaElemento(T elemento, int posicao) {
+        
+        aumentaCapacidade();
         if (this.tamanho < this.elementos.length) {
             for(int i=this.tamanho; i > posicao; i--){
                 this.elementos[i] = this.elementos[i-1];
@@ -76,8 +113,9 @@ public class Vetor {
 
     /**
      * Retorna os elementos do vetor.
+     * @return 
      */
-    public String exibirElementos() {
+    public String exibeTodosElementos() {
 
         StringBuilder s = new StringBuilder(); //Manipulação de Strings.
         s.append("["); //Utilizado para adicionar Strings.
@@ -97,9 +135,12 @@ public class Vetor {
 
     /**
      * Retorna o elemento do vertor indicado pela posição passada por parâmetro.
+     * @param posicaoVetor
+     * @return 
      */
-    public String mostrarElemento(int posicaovetor) {
-        int posicao = posicaovetor;
-        return this.elementos[posicao].toString();
+    public T exibeElemento(int posicaoVetor) {
+        
+        int posicao = posicaoVetor;
+        return this.elementos[posicao];
     }
 }
--- EstruturaDados/src/com/christian/estruturadados/vetor/teste/Teste.java
+++ EstruturaDados/src/com/christian/estruturadados/vetor/teste/Teste.java
@@ -6,7 +6,6 @@
 package com.christian.estruturadados.vetor.teste;
 
 import com.christian.estruturadados.vetor.Vetor;
-import com.christian.ferramentas.Mensagem;
 
 /**
  *
@@ -15,45 +14,58 @@ import com.christian.ferramentas.Mensagem;
 public class Teste {
 
     public static void main(String[] args) {
-
-        Vetor vet = new Vetor(8);
-        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor());
+        
+        Vetor vet = new Vetor();
+        
+System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        vet.adicionarElemento("Adriana");
-        vet.adicionarElemento("Bernardo");
-        vet.adicionarElemento("Christian");
-        vet.adicionarElemento("Diane");
-        vet.adicionarElemento("Eliseu");
-        vet.adicionarElemento("Flávia");
-        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor());
+        vet.adicionaElemento("Adriana");
+        vet.adicionaElemento("Bernardo");
+        vet.adicionaElemento("Christian");
+        vet.adicionaElemento("Diane");
+        vet.adicionaElemento("Eliseu");
+        vet.adicionaElemento("Flávia");
+        vet.adicionaElemento("Flávio");
+        vet.adicionaElemento("Júlio");
+        vet.adicionaElemento("Vitor");
+        vet.adicionaElemento("Kelly");
+        vet.adicionaElemento("José");
+        vet.adicionaElemento("Vanessa");
+        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        System.out.println("Elementos do vetor: " + vet.exibirElementos());
-        System.out.println("o elemento do vetor na posição 2 é " + vet.mostrarElemento(2) + ".\n");
+        System.out.println("Elementos do vetor: " + vet.exibeTodosElementos());
+        System.out.println("o elemento do vetor na posição 2 é " + vet.exibeElemento(2) + ".\n");
         
         System.out.println("Inserir Tiago na posição 0.");
-        vet.adicionarElemento("Tiago", 0);
+        vet.adicionaElemento("Tiago", 0);
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        System.out.println("Elementos do vetor: " + vet.exibirElementos());
+        System.out.println("Elementos do vetor: " + vet.exibeTodosElementos());
+        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         
         System.out.println("Inserir Ana Laura na posição 0.");
-        vet.adicionarElemento("Ana Laura", 0);
+        vet.adicionaElemento("Ana Laura", 0);
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        System.out.println("Elementos do vetor: " + vet.exibirElementos());
+        System.out.println("Elementos do vetor: " + vet.exibeTodosElementos());
+        System.out.println("o elemento do vetor na posição 2 é " + vet.exibeElemento(2) + ".\n");
+        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         
         System.out.println("Inserir Jacob na posição 0.");
-        vet.adicionarElemento("Jacob", 0);
+        vet.adicionaElemento("Jacob", 0);
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        System.out.println("Elementos do vetor: " + vet.exibirElementos());
+        System.out.println("Elementos do vetor: " + vet.exibeTodosElementos());
+        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         
-        System.out.println("Inserir João na posição 0.");
-        vet.adicionarElemento("João", 0);
+        System.out.println("Inserir João na posição 5.");
+        vet.adicionaElemento("João", 5);
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        System.out.println("Elementos do vetor: " + vet.exibirElementos());
+        System.out.println("Elementos do vetor: " + vet.exibeTodosElementos());
+        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         
         System.out.println("Inserir José na posição 0.");
-        vet.adicionarElemento("José", 0);
+        vet.adicionaElemento("José", 0);
         System.out.println("Tamanho do vetor: "+ vet.tamanho());
-        System.out.println("Elementos do vetor: " + vet.exibirElementos());
+        System.out.println("Elementos do vetor: " + vet.exibeTodosElementos());
+        System.out.println("Capacidade total do vetor: "+ vet.capacidadeVetor() +".\n");
         /*
         Mensagem.exibirMensagemErro("Ocorreu um erro!");
         Mensagem.exibirInformação("Isto é uma informação");
